fa9dcc022fa0346667f9ab0641103f6c3bab2401
RedstarTheGnome <jordanredpath@aol.com>
2025-08-07 10:36:41 +0100

    Start button added

--- slideshow/Slideshow.py
+++ slideshow/Slideshow.py
@@ -1,12 +1,21 @@
+import tkinter as tk
 from tkinter import *
-master = Tk()
+from tkinter import messagebox
+master = tk.Tk()
+
+def hello():
+    msg=messagebox.showinfo("hello")
 
 master.title('Auto Slideshow')
 
-Label(master, text='Enter Image file path').grid(row=0)
-e1 = Entry(master)
+tk.Label(master, text='Enter Image file path').grid(row=0)
+e1 = tk.Entry(master)
 e1.grid(row=0, column=1)
 
+
+
+button = tk.Button(master, text='Start Slideshow', width=25, command= hello)
+button.place(x=50,y=50)
 
 
 
