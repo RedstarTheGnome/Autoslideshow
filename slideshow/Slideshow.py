a97f5e81ecc3e9ac7b4e2c4e01a6a8664378dae5
RedstarTheGnome <jordanredpath@aol.com>
2025-08-07 10:26:54 +0100

    Created Tkinter GUI py file

---
+++ slideshow/Slideshow.py
@@ -0,0 +1,13 @@
+from tkinter import *
+master = Tk()
+
+master.title('Auto Slideshow')
+
+Label(master, text='Enter Image file path').grid(row=0)
+e1 = Entry(master)
+e1.grid(row=0, column=1)
+
+
+
+
+master.mainloop()
